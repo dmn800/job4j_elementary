e75bb4de5051db4be6f65c6500ad384ce09b6e5b
dmn800 <nefediev.avid@yandex.ru>
2022-08-27 07:43:42 +0500

    4.4. Вложенный оператор if else. Запутанный код [#190]

---
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class DivideBySix {
+    public static String checkNumber(int number) {
+        String rsl;
+        if (number % 2 == 0 && number % 3 == 0) {
+            rsl = "Number divide by 6.";
+        } else if (number % 3 == 0 && number % 2 != 0) {
+            rsl = "Number divide by 3 and not even.";
+        } else if (number % 3 != 0 && number % 2 == 0) {
+            rsl = "Number no divide by 3 and even.";
+        } else {
+            rsl = "Исходное число не делится на 3 и не является четным.";
+        }
+        return rsl;
+    }
+
+}
---
+++ src/test/java/ru/job4j/condition/DivideBySixTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DivideBySixTest {
+    @Test
+    public void whenNumberDivideBy6() {
+        int in = 24;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Number divide by 6.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberDivideBy3AndNotEven() {
+        int in = 9;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Number divide by 3 and not even.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberNoDivideBy3AndEven() {
+        int in = 14;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Number no divide by 3 and even.";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenNumberNoDivideBy3AndNotEven() {
+        int in = 25;
+        String result = DivideBySix.checkNumber(in);
+        String expected = "Исходное число не делится на 3 и не является четным.";
+        assertThat(result).isEqualTo(expected);
+    }
+}
