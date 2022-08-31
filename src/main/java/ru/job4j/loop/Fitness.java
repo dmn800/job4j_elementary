5a94cebaff44958d420f73fd3f00eb8f57a6224f
dmn800 <nefediev.avid@yandex.ru>
2022-08-31 10:02:27 +0500

    5.4. Протеиновая диета [#156313 #337765]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,9 +4,9 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan <= nik) {
-            month += 1;
-            ivan = ivan * 3 * month;
-            nik = nik * 2 * month;
+            month++;
+            ivan = ivan * 3;
+            nik = nik * 2;
         }
         return month;
     }
