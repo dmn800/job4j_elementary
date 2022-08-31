92ff64dbd84b6f3679251fa51b03de9f907a5d05
dmn800 <nefediev.avid@yandex.ru>
2022-08-31 16:40:10 +0500

    5.7. Ипотека [#156315 #338148]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        amount = amount * (1 + (percent / 100));
         while (amount > 0) {
             year++;
+            amount = amount * (1 + (percent / 100));
             amount = amount - salary;
         }
         return year;
