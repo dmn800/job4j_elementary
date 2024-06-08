ea25867015188aa6f1d4269cdb3bd677d635646b
dmn800 <nefediev.avid@yandex.ru>
2024-06-08 12:54:44 +0300

    Исправление опечатки

--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -14,10 +14,6 @@ public class TurnTest {
 
     @Test
     public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
-        /*
-            напишите здесь тест, провер€ющий переворот массива
-            с нечЄтным числом элементов, например {1, 2, 3, 4, 5}.
-         */
         int[] input = new int[] {1, 2, 3, 4, 5};
         int[] result = Turn.back(input);
         int[] expected = new int[] {5, 4, 3, 2, 1};
